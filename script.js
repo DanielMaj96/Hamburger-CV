fc48174a93f625e1a6990f6b20f24a2d4007aa58
Daniel Maj <daniel.maj96@interia.pl>
2024-02-25 14:12:15 +0100

    Second JS+*

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ layers.forEach((layer, index) => layer.style.top = `${180 - index * 30}px`);
 const unWrap = (el) => {
   const div = el.target.parentNode;
   const divIndex = layers.indexOf(div);
-  // const divPosition = parseInt(div.style.top, 10);
+  const onlyOneOpenClass = layers.filter((layer) => layer.classList.contains('open'));
+  if (onlyOneOpenClass.length > 0 && div.classList.contains('open') === false) {
+    return;
+  }
   div.classList.toggle('open');
-
   if (div.classList.contains('open')) {
     layers.forEach((layer) => {
       const curPosition = parseInt(layer.style.top, 10);
@@ -25,23 +27,5 @@ const unWrap = (el) => {
       }
     });
   }
-
-  // if (div.classList.contains('open')) {
-  //   div.style.top = `${divPosition - 150}px`;
-  //   layers.forEach((layer) => {
-  //     if (layers.indexOf(layer) < divIndex) {
-  //       console.log(divPosition, index);
-  //       layer.style.top = `${divPosition - 150}px`;
-  //       console.log(divPosition, index);
-  //     }
-  //   });
-  // } else {
-  //   div.style.top = `${divPosition + 150}px`;
-  //   layers.forEach((layer) => {
-  //     if (layers.indexOf(layer) < divIndex) {
-  //       layer.style.top = `${divPosition + 150}px`;
-  //     }
-  //   });
-  // }
 };
 layers.forEach((layer) => layer.addEventListener('click', unWrap));
